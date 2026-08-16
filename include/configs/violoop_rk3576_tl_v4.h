b5044ec7c291207fb18f4d38996fdeb947473f71
marvin <marvin@bvio.ai>
2026-08-16 12:50:23 +0000

    board: add Violoop TL V4 U-Boot target
    (cherry picked from commit 7fb12f152b2b9da613ca0762afee6c2774f381a9)

---
+++ include/configs/violoop_rk3576_tl_v4.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+
+#ifndef __CONFIGS_VIOLOOP_RK3576_TL_V4_H
+#define __CONFIGS_VIOLOOP_RK3576_TL_V4_H
+
+#include <configs/rk3576_common.h>
+
+#ifndef CONFIG_SPL_BUILD
+
+#undef ROCKCHIP_DEVICE_SETTINGS
+#ifdef CONFIG_DM_VIDEO
+#define ROCKCHIP_DEVICE_SETTINGS \
+		"stdout=serial,vidconsole\0" \
+		"stderr=serial,vidconsole\0"
+#else
+#define ROCKCHIP_DEVICE_SETTINGS \
+		"stdout=serial\0" \
+		"stderr=serial\0"
+#endif
+
+#define CONFIG_SYS_MMC_ENV_DEV		0
+
+#undef CONFIG_BOOTCOMMAND
+#ifdef CONFIG_DM_VIDEO
+#define CONFIG_BOOTCOMMAND	RKIMG_BOOTCOMMAND
+#else
+#define CONFIG_BOOTCOMMAND	"boot_fit;"
+#endif
+
+#endif /* CONFIG_SPL_BUILD */
+#endif /* __CONFIGS_VIOLOOP_RK3576_TL_V4_H */
